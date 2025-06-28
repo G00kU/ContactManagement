95ff34a118a885c73fd169901e0b259821c6d213
G00KU <gookul18@hotmail.com>
2025-06-28 20:07:15 +0530

    Initial Version of Entity Service

--- ContactServices/Services/IContactService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ContactServices.Modal;
-namespace ContactServices.Services;
-public interface IContactService
-{
-    List<ContactDTO> GetAllContacts();  
-    ContactDTO GetContactById(int contactId); 
-    int InsertorUpdateContact(ContactDTO contact);
-    int DeleteContact(int contactId);
-}
--- contactmanagementapp/src/config/apiconfig.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = "https://localhost:7242/api/";
-
-export const loginMethod = `${baseUrl}Auth/login`;
-
-export const getAllContacts = `${baseUrl}Contact/GetAllContacts`;
-export const getContactById = `${baseUrl}Contact/GetContactById`;
-export const insertOrUpdateContact = `${baseUrl}Contact/InsertorUpdateContact`;
-export const deleteContactById = `${baseUrl}Contact/DeleteContact`;
